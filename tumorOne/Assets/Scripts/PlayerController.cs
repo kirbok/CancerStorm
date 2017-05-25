d12d43ff9edc9eb3ecc1f8a0be60699568f45c81
Gregory Coleman <greglcoleman@gmail.com>
2017-05-24 21:48:19 -0700

    added a broken forward and backward and broke rotation more

--- tumorOne/Assets/Scripts/PlayerController.cs
+++ tumorOne/Assets/Scripts/PlayerController.cs
@@ -5,45 +5,80 @@ using UnityEngine;
 
 
 public class PlayerController : MonoBehaviour {
-	float speed = 6.0f;
-	Quaternion pos;
-	Vector3 forward;
-	Quaternion finalPos;
+	float rotSpeed = 6.0f;
+	float moveSpeed = 6.0f;
+	Quaternion rot, finalRot;
+	Vector3 finalPos, pos;
 	Transform tr;
+	int counter = 0;
 	//float angle;
 
 	// Use this for initialization
 	void Start () {
 
-		pos = transform.rotation;
+		rot = transform.rotation;
+		pos = transform.position;
 		tr = transform;
+		finalRot = rot;
 		finalPos = pos;
+	}
+
+	Vector3 MoveForward() {
+		Vector3 v3 = Vector3.zero;
+		if (Mathf.Approximately(rot.eulerAngles.y,90.0f)) {
+			v3 = new Vector3 (tr.position.x,tr.position.y ,tr.position.z - 10);
+		} else if (Mathf.Approximately(rot.eulerAngles.y,-90.0f)) {
+			v3 = new Vector3 (tr.position.x,tr.position.y ,tr.position.z + 10);
+		} else if (Mathf.Approximately(rot.eulerAngles.y,0.0f)) {
+			v3 = new Vector3 (tr.position.x - 10,tr.position.y ,tr.position.z);
+		} else if (Mathf.Approximately(rot.eulerAngles.y,180.0f)) {
+			v3 = new Vector3 (tr.position.x + 10,tr.position.y ,tr.position.z);
+		}
+		return v3;
+	}
+
+	Vector3 MoveBackward() {
+		Vector3 v3 = Vector3.zero;
+		if (rot.eulerAngles.y == 90) {
+			v3 = new Vector3 (tr.position.x,tr.position.y ,tr.position.z + 10);
+		} else if (rot.eulerAngles.y == 270) {
+			v3 = new Vector3 (tr.position.x,tr.position.y ,tr.position.z - 10);
+		} else if (rot.eulerAngles.y == 0) {
+			v3 = new Vector3 (tr.position.x + 10,tr.position.y ,tr.position.z);
+		} else if (rot.eulerAngles.y == 180) {
+			v3 = new Vector3 (tr.position.x - 10,tr.position.y ,tr.position.z);
+		}
+		return v3;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("d"))
-		{
-			finalPos = Quaternion.Euler(new Vector3(pos.eulerAngles.x, pos.eulerAngles.y + 90, pos.eulerAngles.z));
+		if (counter == 0) {
+			if (Input.GetAxis ("Horizontal") > 0) {
+				finalRot = Quaternion.Euler (new Vector3 (rot.eulerAngles.x, rot.eulerAngles.y + 90, rot.eulerAngles.z));
+				//finalPos = Quaternion.LookRotation(new Vector3(pos.eulerAngles.x, pos.eulerAngles.z, pos.eulerAngles.y + 90));
+				counter = 20;
+			} else if (Input.GetAxis ("Horizontal") < 0) {
+				finalRot = Quaternion.Euler (new Vector3 (rot.eulerAngles.x, rot.eulerAngles.y - 90, rot.eulerAngles.z));
+				//finalPos = Quaternion.LookRotation(new Vector3(pos.eulerAngles.x, pos.eulerAngles.z, pos.eulerAngles.y - 90));
+				counter = 20;
+			} else if (Input.GetAxis ("Vertical") > 0) {
+				finalPos = MoveForward ();
+				counter = 20;
+			} else if (Input.GetAxis ("Vertical") < 0) {
+				finalPos = MoveBackward ();
+				counter = 20;
+			}
+		} else {
+			counter--;
 		}
-		else if (Input.GetKeyDown("a"))
-		{
-			finalPos = Quaternion.Euler(new Vector3(pos.eulerAngles.x, pos.eulerAngles.y - 90, pos.eulerAngles.z));
-		}
-//		else if (Input.GetKey(KeyCode.W) && tr.position == pos)
-//		{
-//			pos += Vector3.forward;
-//		}
-//		else if (Input.GetKey(KeyCode.S) && tr.position == pos)
-//		{
-//			pos += Vector3.back;
-//		}
-//
+		//tr.rotation = Quaternion.Slerp(tr.rotation, finalPos, Time.deltaTime * speed);
 //		//transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
 	}
 
 	void FixedUpdate() {
-		transform.rotation = Quaternion.Slerp(transform.rotation, finalPos, Time.deltaTime * speed);
+		transform.position = Vector3.MoveTowards (pos, finalPos, Time.deltaTime * moveSpeed);
+		tr.rotation = Quaternion.Slerp(tr.rotation, finalRot, Time.deltaTime * rotSpeed);
 		/*if ((finalPos.eulerAngles.y - transform.rotation.eulerAngles.y) < 1 &&  (finalPos.eulerAngles.y - transform.rotation.eulerAngles.y) > -1) {
 			transform.rotation = finalPos;
 		}*/
