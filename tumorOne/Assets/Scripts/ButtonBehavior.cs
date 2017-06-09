3e13680967bee09ce3799a5d0b87774ad7d8e60f
kirbok <dufalom@gmail.com>
2017-06-08 21:46:27 -0700

    Menu Ui
    Menu UI IN progress

---
+++ tumorOne/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour {
+	private GameObject info;
+	// Use this for initialization
+	//private GameObject clearPanel;
+	int clear = 500;
+	void Start () {
+		
+	}
+	
+
+	public void button (int infoDisplay) {
+
+		//this.gameObject.transform.GetChild (clear);
+		info = this.gameObject.transform.GetChild(infoDisplay).gameObject;
+		//clearPanel = this.gameObject.transform.GetChild(clear).gameObject;
+		//clearPanel.SetActive (false);
+
+		info.SetActive (true);
+
+		clear = infoDisplay;
+		
+	}
+}
